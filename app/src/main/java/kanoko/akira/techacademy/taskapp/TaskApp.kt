5103f206c2b8f8fafd5831596ee208fc4326b8fa
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-15 13:52:58 +0900

    Initial commit

---
+++ app/src/main/java/kanoko/akira/techacademy/taskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package kanoko.akira.techacademy.taskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
